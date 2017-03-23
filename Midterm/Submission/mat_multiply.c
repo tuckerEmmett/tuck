975c0f7a61800500c8139630ae40a6f073797268
tuckerEmmett <twemmett99@gmail.com>
2017-03-22 22:27:17 -0600

    V 1.1

---
+++ Midterm/Submission/mat_multiply.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void mat_multiply(int argc, const char *argv[]){
+
+
+
+int test = atoi(argv[1]);
+
+printf("Value returned is %d",test);
+
+
+
+
+return;
+
+}
+
+
+int main(){
+
+return 0;
+
+}
