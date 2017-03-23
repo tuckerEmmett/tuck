40c642810c0153b9624434e05a002ccfcce44f07
tuckerEmmett <twemmett99@gmail.com>
2017-03-23 15:50:41 -0600

    V 2.1

--- Midterm/Submission/mat_multiply.c
+++ Midterm/Submission/mat_multiply.c
@@ -3,15 +3,31 @@
 #include <string.h>
 
 
-void main(int argc, char *argv[]){
+void main(int argc, const char *argv[]){
 printf("Loading in file \n");
 
 
-char *myfile = argv[1];
+FILE *myfile = fopen(argv[1],"r");
+
+int ii = 0;
+int *val = malloc(11);
+fscanf(myfile,"%d",&ii);
 
 
+while (!feof(myfile))
+
+{
+
+printf("%d",ii);
+*val = fscanf(myfile,"%d",&ii);
+
+}
+fclose(myfile);
 
 
+int add = val(1) + val(2);
+
+printf("added value is %d",add);
 
 printf("Exiting code \n");
 return;
