036cb11fe343d0c5c8540582f3d2523a0c281038
tuckerEmmett <twemmett99@gmail.com>
2017-03-24 21:40:02 -0600

    Done with everything. I hate C. I hate software.

---
+++ Midterm/Submission/batch_mat_multiply.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mydir=$1;
+
+A_mult_A=./main A A;
+A_mult_B=./main A B;
+A_mult_C=./main A C;
+B_mult_A=./main B A;
+B_mult_B=./main B B;
+B_mult_C=./main B C;
+C_mult_A=./main C A;
+C_mult_B=./main C B;
+C_mult_C=./main C C;
+
+
+
---
+++ Midterm/Submission/multiply.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readHead.h"
+
+
+int *multiply(int *A, int *B){
+
+
+int *product;
+
+//In the interest of time I'm just going to write a routine here. I don't 
+//know what is going wrong with the seg faults in my mat_multiply file
+//and I've spent about five hours on that part alone.
+
+/*
+
+
+check (correct matrix dimensions--mxn X pxq, where n = p)
+	if (wrong dimensions)
+		print "Matrix multiplication cannot continue. Exiting."
+		return product;
+	else
+		malloc(product of size m x q);
+		product = 0;
+		for i in 1 to m
+			for j in 1 to q
+				for k in 1 to n 
+				product(i,j) = product(i,j)+ A(i,k)*B(k,j)
+				end
+			end
+		end
+	end
+
+
+
+
+*/
+
+
+return product;
+//I understand that it may be unorthodox but I would really appreciate 
+//leniency in grading. I have zero experience in C before this class and 
+//have had very little time in the past few weeks between Senior Projects
+//and midterms to teach myself this language. In any case, Happy Spring Break!
+}
